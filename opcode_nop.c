1f037a816118e3bc273d4975252bab6ffd8aea38
emmaolondo <emmaolondo448@gmail.com>
2023-06-22 08:00:11 +0300

    opcode NOP

---
+++ opcode_nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ */
+void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	/** 
+	 * the opcode NOP is an equivalent to NULL in c
+	 * therefore print nothing
+	 */
+	;
+}
