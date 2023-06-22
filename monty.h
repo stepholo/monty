f1eb4094632dd6d69a5cb53d3af141366f60ad34
stepholo <oloobrian89@gmail.com>
2023-06-22 14:00:02 -0700

    Header File

--- monty.h
+++ monty.h
@@ -48,10 +48,17 @@ void add(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void rotr(stack_t **stack, __attribute__((unused))unsigned int line_number);
+void _stack(stack_t **stack, __attribute((unused))unsigned int line_number);
+void _queue(stack_t **stack, __attribute((unused))unsigned int line_number);
 
 /* Helper functions */
 void print_error(unsigned int line_number, const char *message);
 int is_integer(const char *str);
-int process_instructions(FILE *file, stack_t *stack);
+int process_instructions(FILE *file, stack_t **stack);
+void cleanup(stack_t *stack);
 
 #endif /* MONTY_H */
